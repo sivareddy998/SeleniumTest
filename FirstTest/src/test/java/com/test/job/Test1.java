798886b067df450d745d58e3a20ce19ff329110d
sivareddy <sivareddy@sivareddy-PC>
2020-04-15 18:40:14 +0530

    Code is added

---
+++ FirstTest/src/test/java/com/test/job/Test1.java
@@ -0,0 +1,18 @@
+package com.test.job;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+          for(int i=0;i<=10;i++) {
+			
+			System.out.println(i);
+		}
+		    System.out.println("Hello World");
+
+	}
+
+
+	}
+
+
